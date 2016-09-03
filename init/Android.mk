584f31e70cb1c8449caf369587a5646c81648f6a
Neil Agarwal <regalstreak@gmail.com>
2016-09-03 18:33:24 +0200

    athene: init | Do not build libinit_athene for now

--- init/Android.mk
+++ init/Android.mk
@@ -1,12 +1,12 @@
-LOCAL_PATH := $(call my-dir)
+#LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+#include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
-LOCAL_CPPFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_athene.cpp
-LOCAL_MODULE := libinit_athene
-LOCAL_STATIC_LIBRARIES := libcutils libc
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_C_INCLUDES := system/core/init
+#LOCAL_CPPFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
+#LOCAL_SRC_FILES := init_athene.cpp
+#LOCAL_MODULE := libinit_athene
+#LOCAL_STATIC_LIBRARIES := libcutils libc
 
-include $(BUILD_STATIC_LIBRARY)
+#include $(BUILD_STATIC_LIBRARY)
